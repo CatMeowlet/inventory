a16813b269e5e98190411016d64581678cfec15f
james hipe <jameszzz999888@gmail.com>
2020-01-14 17:49:30 +0800

    added some add

--- config/app.php
+++ config/app.php
@@ -175,6 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         SimpleSoftwareIO\QrCode\QrCodeServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
     ],
 
     /*
@@ -226,7 +227,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'QrCode' => SimpleSoftwareIO\QrCode\Facades\QrCode::class,
-
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
     ],
 
 ];
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,12 +9,30 @@
 
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
 
-                    You are logged in!
+                    <div class="row">
+                        <div class="container">
+                            <div class="card-header">Add Inventory</div>
+                            <div class="card-body">
+                                {!! Form::open(['action' => 'HomeController@store', 'method' => 'POST']) !!}
+                                <div class="form-group">
+                                    {{Form::label('item_id','Item ID')}}
+                                    {{Form::text('item_id','', ['class' => 'form-control', 'placeholder' => ''])}}
+                                </div>
+                                <div class="form-group">
+                                    {{Form::label('user_id','User ID')}}
+                                    {{Form::text('user_id','', ['class' => 'form-control', 'placeholder' => ''])}}
+                                </div>
+                                {{Form::submit('Submit', ['class'=>'btn btn-primary'])}}
+                                {!! Form::close() !!}
+                            </div>
+                            <!-- /.cardbody for add -->
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
